4dd1990a80607b0c95595914315241945de1db69
Abdelrahman <88583085+abdel-17@users.noreply.github.com>
2023-04-26 13:43:40 +0200

    Code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,18 @@ import Index from "./pages/index/Index";
 import Login from "./pages/login/Login";
 import Contact from "./pages/contact/Contact";
 import Root from "./pages/Root";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SkeletonTheme } from "react-loading-skeleton";
 
 function App() {
-  const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <SkeletonTheme baseColor="#DDDBDD">
-        <BrowserRouter>
-          <Root>
-            <Routes>
-              <Route index element={<Index />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </Root>
-        </BrowserRouter>
-      </SkeletonTheme>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Root>
+        <Routes>
+          <Route index element={<Index />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Root>
+    </BrowserRouter>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SkeletonTheme } from "react-loading-skeleton";
 
+const queryClient = new QueryClient();
 const root = document.getElementById("root") as HTMLElement;
+
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <SkeletonTheme baseColor="#DDDBDD">
+        <App />
+      </SkeletonTheme>
+    </QueryClientProvider>
   </React.StrictMode>
 );
