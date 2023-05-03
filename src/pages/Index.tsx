366c4a0d7510a6ddf43ca53e06dc4cbf61286882
Abdelrahman <88583085+abdel-17@users.noreply.github.com>
2023-05-04 01:32:48 +0300

    Filter chips now collapse to a menu on mobile

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,16 +1,21 @@
 import {
   Box,
-  Button,
   Chip,
+  Container,
   Grid,
+  IconButton,
+  Menu,
+  MenuItem,
   Paper,
   Stack,
+  Tooltip,
   Typography,
   useTheme,
 } from "@mui/material";
 import ErrorFallback from "../components/ErrorFallback";
 import { Product, useProducts } from "../hooks/products";
 import { useState } from "react";
+import { FilterList as FilterListIcon } from "@mui/icons-material";
 
 const categories = ["electronics", "jewelery", "men's clothing", "women's clothing"];
 
@@ -23,70 +28,129 @@ export default function Index() {
     return <ErrorFallback error={products.error} onRetry={products.refetch} />;
   }
 
-  const brandStyle = { color: palette.primary.light };
+  function FilterMenu() {
+    const [anchor, setAnchor] = useState<HTMLElement | null>(null);
+
+    const onOpen = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchor(event.currentTarget);
+    };
+    const onClose = () => {
+      setAnchor(null);
+    };
+
+    return (
+      <Box
+        display={{
+          xs: "flex",
+          sm: "none",
+        }}
+        justifyContent="end"
+        marginBottom={1}
+      >
+        <Tooltip title="Filter">
+          <IconButton onClick={onOpen}>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+
+        <Menu
+          anchorEl={anchor}
+          open={anchor !== null}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          onClose={onClose}
+        >
+          {categories.map((category) => (
+            <MenuItem
+              selected={category === selectedCategory}
+              onClick={() => {
+                setSelectedCategory((current) =>
+                  category === current ? null : category
+                );
+                onClose();
+              }}
+            >
+              <span style={{ textTransform: "capitalize" }}>{category}</span>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
+    );
+  }
+
+  function FilterChips() {
+    return (
+      <Stack
+        display={{
+          xs: "none",
+          sm: "flex",
+        }}
+        direction="row"
+        justifyContent="center"
+        spacing={1}
+        marginBottom={2}
+      >
+        {categories.map((category) => {
+          const isSelected = category === selectedCategory;
+          return (
+            <Chip
+              variant="filled"
+              label={<span style={{ textTransform: "capitalize" }}>{category}</span>}
+              color={isSelected ? "secondary" : "default"}
+              onClick={() => setSelectedCategory(category)}
+              onDelete={isSelected ? () => setSelectedCategory(null) : undefined}
+            />
+          );
+        })}
+      </Stack>
+    );
+  }
 
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" padding={4}>
+    <Box padding={4}>
       <Typography component="h1" variant="h4" textAlign="center" fontWeight="bold">
-        Welcome to <span style={brandStyle}>Shopmania!</span>
+        Welcome to <span style={{ color: palette.primary.light }}>Shopmania!</span>
       </Typography>
 
-      <Typography component="h2" variant="h5" textAlign="center" marginTop={2}>
+      <Typography
+        component="h2"
+        variant="h5"
+        textAlign="center"
+        marginTop={2}
+        marginBottom={4}
+      >
         Browse our diverse product catalog
       </Typography>
 
       {products.isLoading ? (
         <LoadingFallback />
       ) : (
-        <>
-          <Stack direction="row" spacing={2} marginTop={4}>
-            {categories.map((category) => (
-              <FilterChip
-                key={category}
-                category={category}
-                isSelected={selectedCategory === category}
-                setSelectedCategory={setSelectedCategory}
-              />
-            ))}
-          </Stack>
+        <Container maxWidth="md">
+          <FilterMenu />
+
+          <FilterChips />
 
           <ProductsGrid
             products={products.data.filter(
               (product) => !selectedCategory || product.category === selectedCategory
             )}
           />
-        </>
+        </Container>
       )}
     </Box>
-  );
-}
-
-function FilterChip(props: {
-  category: string;
-  isSelected: boolean;
-  setSelectedCategory: (category: string | null) => void;
-}) {
-  const { category, isSelected, setSelectedCategory } = props;
-
-  const onClick = () => setSelectedCategory(category);
-  const onDelete = isSelected ? () => setSelectedCategory(null) : undefined;
-
-  const labelStyle = { textTransform: "capitalize" } as const;
-  return (
-    <Chip
-      label={<span style={labelStyle}>{category}</span>}
-      color="secondary"
-      variant={isSelected ? "filled" : "outlined"}
-      onClick={onClick}
-      onDelete={onDelete}
-    />
   );
 }
 
 function ProductsGrid(props: { products: Product[] }) {
   const { products } = props;
   return (
-    <Grid container maxWidth="md" spacing={2} marginTop={1}>
+    <Grid container spacing={2}>
       {products.map((product) => (
         <Grid key={product.id} item xs={12} sm={6} md={4}>
           <Paper
@@ -94,7 +158,7 @@ function ProductsGrid(props: { products: Product[] }) {
               padding: 2,
               background: "white",
               transition: "0.5s",
-              "&:hover": {
+              ":hover": {
                 scale: "1.05",
               },
             }}
