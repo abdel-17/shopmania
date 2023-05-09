d0d67d2714c9b365467e4c8f4dbade145b87bdff
Abdelrahman <88583085+abdel-17@users.noreply.github.com>
2023-05-10 02:41:24 +0300

    Added logo + responsive drawer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,32 +1,115 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
-import { AppBar, Box, Button, IconButton, Toolbar, Tooltip } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Tooltip,
+} from "@mui/material";
 import {
   Login as LoginIcon,
   Logout as LogoutIcon,
   ShoppingCartOutlined as OutlinedCartIcon,
   ShoppingCart as CartIcon,
+  Menu as MenuIcon,
+  Store as StoreIcon,
+  StoreOutlined as StoreOutlinedIcon,
+  SupportAgent as SupportAgentIcon,
+  SupportAgentOutlined as SupportAgentOutlinedIcon,
+  Info as InfoIcon,
+  InfoOutlined as InfoOutlinedIcon,
 } from "@mui/icons-material";
 import supabase from "../supabase/client";
 import { useSession } from "./SessionProvider";
+import Logo from "./Logo";
 
 export default function Layout() {
   const session = useSession();
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  const onShowDrawer = () => setShowDrawer(true);
+  const onHideDrawer = () => setShowDrawer(false);
+
   return (
     <>
       <AppBar component="header">
-        <Toolbar>
-          <Box component="nav" display="flex" flexGrow={1}>
-            <NavLink to="/products">Products</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact Us</NavLink>
+        <Toolbar sx={{ position: "relative" }}>
+          <Box width="100%">
+            <Box
+              component="nav"
+              display={{
+                xs: "none",
+                md: "inherit",
+              }}
+            >
+              <NavLink to="/products">Products</NavLink>
+              <NavLink to="/about">About</NavLink>
+              <NavLink to="/contact">Contact Us</NavLink>
+            </Box>
+
+            <IconButton
+              onClick={onShowDrawer}
+              sx={{
+                display: {
+                  md: "none",
+                },
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
           </Box>
 
-          {!session && <LoginButton />}
-          {session && <CartButton />}
-          {session && <LogoutButton />}
+          <Logo
+            width={140}
+            style={{
+              position: "absolute",
+              left: "calc(50% - 70px)",
+            }}
+          />
+
+          {session ? (
+            <Fragment>
+              <CartButton />
+              <LogoutButton />
+            </Fragment>
+          ) : (
+            <LoginButton />
+          )}
         </Toolbar>
       </AppBar>
+
+      <Drawer open={showDrawer} onClose={onHideDrawer}>
+        <List sx={{ width: 200, paddingTop: 2 }}>
+          <ListItemLink
+            to="/products"
+            Icon={<StoreOutlinedIcon />}
+            SelectedIcon={<StoreIcon />}
+            label="Products"
+            onClick={onHideDrawer}
+          />
+          <ListItemLink
+            to="/about"
+            Icon={<InfoOutlinedIcon />}
+            SelectedIcon={<InfoIcon />}
+            label="About"
+            onClick={onHideDrawer}
+          />
+          <ListItemLink
+            to="/contact"
+            Icon={<SupportAgentOutlinedIcon />}
+            SelectedIcon={<SupportAgentIcon />}
+            label="Contact Us"
+            onClick={onHideDrawer}
+          />
+        </List>
+      </Drawer>
 
       <Toolbar />
 
@@ -97,5 +180,23 @@ function CartButton() {
         {isSelected ? <CartIcon /> : <OutlinedCartIcon />}
       </IconButton>
     </Tooltip>
+  );
+}
+
+function ListItemLink(props: {
+  to: string;
+  Icon: React.ReactNode;
+  SelectedIcon: React.ReactNode;
+  label: string;
+  onClick: () => void;
+}) {
+  const { to, Icon, SelectedIcon, label, onClick } = props;
+  const { pathname } = useLocation();
+  const isSelected = to === pathname;
+  return (
+    <ListItemButton component={Link} to={to} selected={isSelected} onClick={onClick}>
+      <ListItemIcon>{isSelected ? SelectedIcon : Icon}</ListItemIcon>
+      <ListItemText>{label}</ListItemText>
+    </ListItemButton>
   );
 }
