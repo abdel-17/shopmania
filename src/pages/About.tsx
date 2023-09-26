489fdd8a42c5f8b95479ff1523655359af7a984c
abdel-17 <abdelrahmanb015@gmail.com>
2023-09-26 08:39:53 +0300

    Change layout & typography

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,60 +1,51 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 import TeamIcon from "../assets/team.svg?react";
 
 export function About() {
   return (
-    <Box
+    <Stack
       className="fullscreen"
-      display="flex"
+      direction={{
+        xs: "column",
+        md: "row-reverse",
+      }}
       alignItems="center"
       justifyContent="center"
+      padding={3}
+      spacing={4}
     >
-      <Stack
-        direction={{
-          xs: "column",
-          lg: "row-reverse",
-        }}
-        margin={4}
-        spacing={4}
-      >
-        <TeamIcon
-          style={{
-            maxWidth: 400,
-            alignSelf: "center",
-          }}
-        />
+      <TeamIcon style={{ maxWidth: 300 }} />
 
-        <Box maxWidth="md">
-          <Typography
-            component="h1"
-            variant="h3"
-            textAlign="center"
-            fontWeight="bold"
-          >
-            About Us
-          </Typography>
+      <div>
+        <Typography
+          component="h1"
+          variant="h4"
+          textAlign="center"
+          fontWeight="bold"
+        >
+          About Us
+        </Typography>
 
-          <Typography fontSize={18} marginTop={3} letterSpacing={1}>
-            Welcome to Shopmania, the ultimate online shopping destination! Our
-            mission is to provide you with a seamless and enjoyable shopping
-            experience, where you can find everything you need in one place.
-            <br />
-            <br />
-            At Shopmania, we believe that shopping should be easy, affordable,
-            and fun. That's why we have carefully curated a selection of
-            high-quality products from top brands and reliable suppliers, all at
-            competitive prices.
-            <br />
-            <br />
-            We are committed to providing top-notch customer service, and we
-            strive to make your shopping experience as smooth and hassle-free as
-            possible. Our website is easy to navigate, and our secure checkout
-            process ensures that your personal and financial information is
-            always protected.
-          </Typography>
-        </Box>
-      </Stack>
-    </Box>
+        <Typography maxWidth="sm" variant="body1" marginTop={2}>
+          Welcome to Shopmania, the ultimate online shopping destination! Our
+          mission is to provide you with a seamless and enjoyable shopping
+          experience, where you can find everything you need in one place.
+          <br />
+          <br />
+          At Shopmania, we believe that shopping should be easy, affordable, and
+          fun. That's why we have carefully curated a selection of high-quality
+          products from top brands and reliable suppliers, all at competitive
+          prices.
+          <br />
+          <br />
+          We are committed to providing top-notch customer service, and we
+          strive to make your shopping experience as smooth and hassle-free as
+          possible. Our website is easy to navigate, and our secure checkout
+          process ensures that your personal and financial information is always
+          protected.
+        </Typography>
+      </div>
+    </Stack>
   );
 }
