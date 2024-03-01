6e2d2d08ffe1cf0f5f3f3fc92fd0ff0fda27c29c
abdel-17 <abdelrahmanb015@gmail.com>
2024-03-01 05:09:44 +0200

    update `material-ripple-web` dep

--- my-remix-app/app/components/Ripple.tsx
+++ my-remix-app/app/components/Ripple.tsx
@@ -1,7 +1,2 @@
 import "material-ripple-web/ripple.css";
-import { useRippleRef } from "material-ripple-web/react";
-
-export function Ripple(props: { disabled?: boolean }) {
-	const ref = useRippleRef<HTMLDivElement>(props);
-	return <div ref={ref} />;
-}
+export { Ripple } from "material-ripple-web/react";
